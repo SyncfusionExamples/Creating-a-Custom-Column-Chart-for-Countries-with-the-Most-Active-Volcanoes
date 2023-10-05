9cc0c81e308ede5e17d458cdfa4c4cde8b8eeed9
SaiyathAliFathima <saiyathalifathimabee.m@syncfusion.com>
2023-10-05 13:08:51 +0530

    Added the sample

---
+++ MostActiveVolcanoes/MostActiveVolcanoes/Model/VolcanoesModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace MostActiveVolcanoes
+{
+    public class VolcanoesModel
+    {
+        public string Country { get; set; }
+        public int Count { get; set; }
+
+        public VolcanoesModel(string country, int count)
+        {
+            Country = country;
+            Count = count;
+        }
+    }
+}
